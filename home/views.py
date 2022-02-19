12ad3ed4e41aba630c8dc58fbf9d77ce275c64c1
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-02-19 21:52:38 +0530

    added and migrated for home app model

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . models import AppList
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    apps = AppList.objects
+    return render(request, 'home/home.html', {'apps': apps})
