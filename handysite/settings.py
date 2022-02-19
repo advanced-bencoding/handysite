1827dc42109e1b47c65e84587a2fcfb89228d267
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-02-19 21:50:28 +0530

    added static path

--- handysite/settings.py
+++ handysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.apps.AppsConfig',
     'calculator.apps.CalculatorConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
@@ -124,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'handysite/static')]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
