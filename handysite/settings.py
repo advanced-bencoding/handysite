836a2c277e1abbb000e273b055218a61cd27b8b5
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-02-19 19:27:53 +0530

    updated settings.py to include newly added apps

--- handysite/settings.py
+++ handysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.apps.AppsConfig',
+    'calculator.apps.CalculatorConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'handysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['handysite/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
