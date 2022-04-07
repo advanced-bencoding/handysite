ecc79f4ce9cb802546b925cf619a40bad7caa08d
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-04-07 17:07:16 +0530

    added custom regex check and valid ip check

---
+++ regexchecker/static/regexchecker/regex.js
@@ -0,0 +1,36 @@
+const customString = document.getElementById("custom");
+const customRegex = document.getElementById("custom-regex");
+const customSubmit = document.getElementById("custom-submit");
+const customResult = document.getElementById("custom-result");
+const ip = document.getElementById("ip");
+const ipCheck = document.getElementById("ip-submit");
+const ipResult = document.getElementById("ip-result");
+
+customSubmit.addEventListener("click", ()=>{
+    const myString = customString.value;
+    const caseFlag = document.getElementById("ignore-case").checked;
+    const globalFlag = document.getElementById("global").checked;
+    let myRegex;
+    let result;
+    if(caseFlag || globalFlag){
+        let flag = "";
+        if(caseFlag){
+            flag+="i";
+        }
+        if(globalFlag){
+            flag+="g";
+        }
+        myRegex = new RegExp(customRegex.value, flag);
+    }
+    else{
+        myRegex = new RegExp(customRegex.value);
+    }
+
+    customResult.value = myRegex.test(myString);
+})
+
+ipCheck.addEventListener("click", ()=>{
+    const myIP = ip.value;
+    const regex = /^([1-9][0-9]*)[.]([1-9][0-9]*|[0])[.]([1-9][0-9]*|[0])[.]([1-9][0-9]*|[0])$/;
+    ipResult.value = regex.test(myIP);
+})
