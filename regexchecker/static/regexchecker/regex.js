158d995180f6961affb49fbdee735f9d10000e1c
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-04-08 22:37:18 +0530

    strong password checker logic implemented

--- regexchecker/static/regexchecker/regex.js
+++ regexchecker/static/regexchecker/regex.js
@@ -7,6 +7,8 @@ const ipCheck = document.getElementById("ip-submit");
 const ipResult = document.getElementById("ip-result");
 const pass = document.getElementById("password");
 const checkPass = document.getElementById("password-submit");
+const form = document.getElementById("passcheckboi");
+const passLength = document.getElementById("pass-length");
 
 const def = document.getElementById("check-default");
 def.checked = true;
@@ -45,6 +47,20 @@ ipCheck.addEventListener("click", () => {
   ipResult.value = regex.test(myIP);
 });
 
+//Default validation stuff
+let passlen = 8;
+let upperlen = 0;
+let lowerlen = 0;
+let speciallen = 0;
+let numericlen = 0;
+let defregex = /(?=.*[a-z]{1,})(?=.*[A-Z]{1,})(?=.*[0-9]{1,})(?=.*[$&+,:;=?@#|'<>.*()%!]{1,})[a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{8,}/;
+//let newRegex = `(?=.*[a-z]{${lowerlen},})(?=.*[A-Z]{${upperlen},})(?=.*[0-9]{${numericlen},})(?=.*[$&+,:;=?@#|'<>.*()%!]{${speciallen},})[a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{${passlen},}`;
+let newRegexObj;
+
+function generateRegex(newRegex){
+  newRegexObj = new RegExp(newRegex);
+}
+
 def.addEventListener("click", () => {
   if (def.checked) {
     length.checked = false;
@@ -52,6 +68,11 @@ def.addEventListener("click", () => {
     lower.checked = false;
     special.checked = false;
     numeric.checked = false;
+    passlen = 8;
+    upperlen = 0;
+    lowerlen = 0;
+    speciallen = 0;
+    numericlen = 0;
   }
 });
 
@@ -61,18 +82,42 @@ length.addEventListener("click", () => {
 
 upper.addEventListener("click", () => {
   defstatus();
+  if(upper.checked){
+    upperlen = 1;
+  }
+  else{
+    upperlen = 0;
+  }
 });
 
 lower.addEventListener("click", () => {
   defstatus();
+  if(lower.checked){
+    lowerlen = 1;
+  }
+  else{
+    lowerlen = 0;
+  }
 });
 
 special.addEventListener("click", () => {
   defstatus();
+  if(special.checked){
+    speciallen = 1;
+  }
+  else{
+    speciallen = 0;
+  }
 });
 
 numeric.addEventListener("click", () => {
   defstatus();
+  if(numeric.checked){
+    numericlen = 1;
+  }
+  else{
+    numericlen = 0;
+  }
 });
 
 const defstatus = () => {
@@ -82,6 +127,14 @@ const defstatus = () => {
 };
 
 checkPass.addEventListener("click", ()=>{
+  if(length.checked){
+    passlen = passLength.value;
+  }
+  else{
+    passlen = 8;
+  }
+  let newRegex = `(?=.*[a-z]{${lowerlen},})(?=.*[A-Z]{${upperlen},})(?=.*[0-9]{${numericlen},})(?=.*[$&+,:;=?@#|'<>.*()%!]{${speciallen},})[a-zA-Z0-9$&+,:;=?@#|'<>.-^*()%!]{${passlen},}`;
+  generateRegex(newRegex);
+  let str = pass.value;
+  alert(newRegexObj.test(str));
 })
-
-pass.setCustomValidity("hello");
